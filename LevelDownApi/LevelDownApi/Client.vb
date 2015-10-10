8937c9dea10eb7ca989a1114199c07d339ee9e6f
maxerbox <dualprodu@gmail.com>
2015-10-10 12:38:23 +0200

    Modification des methodes

--- LevelDownApi/LevelDownApi/Client.vb
+++ LevelDownApi/LevelDownApi/Client.vb
@@ -2,73 +2,115 @@
 Namespace LevelDown
     Public Class Client
 
-        Public Async Function LoginAsync(ByVal username As String, ByVal password As String) As Task(Of WebTypeResult.WebyBaseResult)
-            Dim httpclient As New System.Net.WebClient()
-            httpclient.BaseAddress = "http://leveldown.fr/"
-            Dim Data As New Specialized.NameValueCollection
-            Dim Message As New WebTypeResult.WebyBaseResult()
-            Dim TokenResult = GetTokenAsync().Result
-            MsgBox(TokenResult.Token)
-            With Data
-                .Add("tk", GetTokenAsync().Result.Token)
-                .Add("login_username", username)
-                .Add("login_password", password)
-            End With
-            Try
-                Await httpclient.UploadValuesTaskAsync("http://leveldown.fr/connexion", Data)
-            Catch ex As System.Net.WebException
-                Message = New WebTypeResult.WebyBaseResult(True, ex.Message, ex.Status)
-            End Try
-            Return Message
-        End Function
-        Private Async Function GetTokenAsync() As Task(Of WebTokenResult)
-
-            Return Await Task.Run(Function() As WebTokenResult
-                                      Dim message As New WebTokenResult
+        Public Async Function LoginAsync(ByVal username As String, ByVal password As String) As Task(Of WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType))
+            Return Await Task.Run(Function() As WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType)
                                       Dim httpclient As New System.Net.WebClient()
+                                      httpclient.BaseAddress = "http://leveldown.fr/"
+
+                                      Dim Data As New Specialized.NameValueCollection
+                                      Dim Message As New WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType)
+                                      Message = GetTokenAsync().Result
+                                      InputBox("Token",, Message.Result.Token)
+                                      With Data
+                                          .Add("tk", Message.Result.Token)
+                                          .Add("login_username", username)
+                                          .Add("login_password", password)
+                                      End With
+                                      Try
+                                          httpclient.UploadValues("http://leveldown.fr/connexion", Data)
+                                          Dim SetCookie = httpclient.ResponseHeaders.GetValues("Set-Cookie").ElementAt(0)
+                                          Dim session As String = SetCookie.Substring(SetCookie.IndexOf("sess=") + 5, SetCookie.IndexOf(";", SetCookie.IndexOf("sess=") + 5) - (SetCookie.IndexOf("sess=") + 5))
+                                          Message.Result.Session = session
+                                          InputBox("Session",, Message.Result.Session)
+                                      Catch ex As System.Net.WebException
+                                          MsgBox(ex.ToString)
+                                          Message = New WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType)(True, ex.Message, ex.Status)
+                                      End Try
+
+                                      Return Message
+                                  End Function)
+        End Function
+        Private Async Function GetTokenAsync() As Task(Of WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType))
+
+            Return Await Task.Run(Function() As WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType)
+                                      Dim message As New WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType)
+                                      Dim httpclient As New Net.WebClient()
                                       Dim source = ""
                                       Try
                                           source = httpclient.DownloadString("http://leveldown.fr/connexion")
                                       Catch ex As System.Net.WebException
-                                          message = New WebTokenResult(True, ex.Message, ex.Status)
+                                          message = New WebTypeResult.WebyBaseResult(Of Types.WebTokenSessionType)(True, ex.Message, ex.Status)
                                       End Try
                                       If message.IsErronous = False Then
                                           Dim Mot1 As String = "value="""
                                           Dim indexofmot1 As Integer = source.IndexOf(Mot1, source.IndexOf("name=""tk"""))
                                           Dim indexoflastquote = source.IndexOf("""", indexofmot1 + Mot1.Length)
                                           Dim resultat = source.Substring(indexofmot1 + Mot1.Length, indexoflastquote - (indexofmot1 + Mot1.Length))
-                                          message.Token = resultat
+                                          message.Result.Token = resultat
                                       End If
                                       Return message
                                   End Function)
 
         End Function
-        Private Class WebTokenResult
-            Inherits WebTypeResult.WebyBaseResult
-            Private _Token As String = ""
-            Public Property Token() As String
-                Get
-                    Return _Token
-                End Get
-                Set(ByVal value As String)
-                    _Token = value
-                End Set
-            End Property
 
-            Public Sub New(ByVal __IsErronous As Boolean, ByVal __message As String, ByVal __status As Net.WebExceptionStatus)
-                MyBase.New(__IsErronous, __message, __status)
-            End Sub
-            Public Sub New(ByVal __token As String)
-                MyBase.New
-                _Token = __token
-            End Sub
-            Public Sub New()
-                MyBase.New
-            End Sub
-
-        End Class
     End Class
     Namespace WebTypeResult
+        Public Class WebyBaseResult(Of TResult As {Class, New})
+            Private _TResult As New TResult
+            Public Property Result() As TResult
+                Get
+                    Return _TResult
+                End Get
+                Set(ByVal value As TResult)
+                    _TResult = value
+                End Set
+            End Property
+            Private _IsErronous As Boolean
+            Public Property IsErronous() As Boolean
+                Get
+                    Return _IsErronous
+                End Get
+                Set(ByVal value As Boolean)
+                    _IsErronous = value
+                End Set
+            End Property
+            Private _Message As String
+            Public Property Message() As String
+                Get
+                    Return _Message
+                End Get
+                Set(ByVal value As String)
+                    _Message = value
+                End Set
+            End Property
+            Private _Status As Net.WebExceptionStatus
+            Public Property Status() As Net.WebExceptionStatus
+                Get
+                    Return _Status
+                End Get
+                Set(ByVal value As Net.WebExceptionStatus)
+                    _Status = value
+                End Set
+            End Property
+            Public Sub New(ByVal __message As String)
+                _IsErronous = False
+                _Message = __message
+                _Status = Net.WebExceptionStatus.Success
+            End Sub
+            Public Sub New(ByVal __IsErronous As Boolean, ByVal __message As String, ByVal __status As Net.WebExceptionStatus)
+                _IsErronous = __IsErronous
+                _Message = __message
+                _Status = __status
+            End Sub
+            Public Sub New()
+                _IsErronous = False
+                _Message = "Ok"
+                _Status = Net.WebExceptionStatus.Success
+
+            End Sub
+        End Class
+
+
         Public Class WebyBaseResult
             Private _IsErronous As Boolean
             Public Property IsErronous() As Boolean
@@ -114,6 +156,37 @@ Namespace LevelDown
             End Sub
         End Class
 
+    End Namespace
+    Namespace Types
+        Public Class WebTokenSessionType
+            Private _Token As String = ""
+            Public Property Token() As String
+                Get
+                    Return _Token
+                End Get
+                Set(ByVal value As String)
+                    _Token = value
+                End Set
+            End Property
+            Private _Session As String = ""
+            Public Property Session() As String
+                Get
+                    Return _Session
+                End Get
+                Set(ByVal value As String)
+                    _Session = value
+                End Set
+            End Property
+            Public Sub New(ByVal __token As String)
 
+                _Token = __token
+            End Sub
+            Public Sub New(ByVal __token As String, ByVal __Session As String)
+                _Token = __token
+                _Session = __Session
+            End Sub
+            Public Sub New()
+            End Sub
+        End Class
     End Namespace
 End Namespace
